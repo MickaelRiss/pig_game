ccd63fbf69ff85402893f063d7a4b27997a89839
Mickael Riss <mendos.prod@gmail.com>
2026-01-18 01:34:53 +0100

    feat: finish mini game

---
+++ script.ts
@@ -0,0 +1,70 @@
+const diceImage = document.querySelector(".dice") as HTMLImageElement;
+const newGameBtn = document.querySelector(".btn--new");
+const rollBtn = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
+
+let activePlayer = 0;
+const player1 = document.querySelector(".player--0") as HTMLElement;
+const player2 = document.querySelector(".player--1") as HTMLElement;
+
+function getActivePlayer():HTMLElement {
+    return activePlayer === 0 ? player1 : player2;
+}
+
+function switchPlayer() {
+    document.querySelector(".player--active")!.classList.remove("player--active");
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    getActivePlayer().classList.add("player--active");
+}
+
+function rollDice(): number {
+    const dice: number = Math.floor(Math.random() * 6 + 1);
+    return dice;
+}
+
+function displayDice(dice: number) {
+    diceImage!.classList.remove("hidden");
+    diceImage!.src = `images/dice-${String(dice)}.png`;
+}
+
+function updateCurrentScore(dice: number) {
+    const currentScore = document.getElementById(`current--${activePlayer}`);
+    let score: number = Number(currentScore!.textContent);
+    score += dice;
+    currentScore!.textContent = String(score);
+}
+
+function updateScore() {
+    const scorePlayer = document.getElementById(`score--${activePlayer}`);
+    const currentDicePlayer = document.getElementById(`current--${activePlayer}`);
+    let score: number = Number(scorePlayer!.textContent);
+    let currentDice: number = Number(currentDicePlayer!.textContent);
+    score += currentDice;
+    scorePlayer!.textContent = String(score);
+    currentDicePlayer!.textContent = "0";
+}
+
+rollBtn!.addEventListener("click", () => {
+    const dice: number = rollDice();
+    displayDice(dice);
+    if (dice === 1) {
+        document.getElementById(`score--${activePlayer}`)!.textContent = "0";
+        document.getElementById(`current--${activePlayer}`)!.textContent = "0";
+        return switchPlayer();
+    }
+    updateCurrentScore(dice);
+});
+
+holdBtn!.addEventListener("click", () => {
+    updateScore();
+    switchPlayer();
+});
+
+newGameBtn!.addEventListener("click", () => {
+    activePlayer = 0;
+    diceImage!.classList.add("hidden");
+    document.querySelector("#score--0")!.textContent = "0";
+    document.querySelector("#score--1")!.textContent = "0";
+    document.querySelector("#current--0")!.textContent = "0";
+    document.querySelector("#current--1")!.textContent = "0";
+});
